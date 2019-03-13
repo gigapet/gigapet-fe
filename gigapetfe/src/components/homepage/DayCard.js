a74a58e6ad79bec327415bb5e4d6764efe868d12
McTrip777 <mcjakedude@gmail.com>
2019-03-13 16:03:23 -0700

    styling

--- gigapetfe/src/components/homepage/DayCard.js
+++ gigapetfe/src/components/homepage/DayCard.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const Form = styled.form`
+const MealWrapper = styled.div`
     padding-top: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 80%;
+`;
+
+const ChildForm = styled.form`
+    padding: 5rem;
     display: flex;
     flex-direction: column;
     align-items: center;
     background-color: rgb(116, 116, 145);
     height: 100vh;
+    border-left: 2px solid black;
 `;
 
 const Input = styled.input`
@@ -17,25 +27,30 @@ const Input = styled.input`
     font-size: 1.8rem;
     padding: 1rem;
     margin: 1rem;
-    border-radius: .6rem;
+    border-radius: 1rem;
     border: 2px solid black;
     text-align: left;
 `;
 
 const Div = styled.div`
-    display: flex;    
+display: flex;
+flex-wrap: wrap;
 `;
 
 const Box = styled.div`
-    border: 2px solid black;
-    display: flex;
-    flex-direction: column;
-    width:15rem;
-    align-content:center;
+border: 2px solid black;
+display: flex;
+flex-direction: column;
+margin: 3rem;
+width: 25rem;
+height: 25rem;
+justify-content: center;
+background: gray;
 `;
 
 const P = styled.p`
     padding: 1rem 5rem;
+    margin: 0rem;
 `;
 
 const Select = styled.select`
@@ -47,11 +62,14 @@ const Select = styled.select`
     border: 2px solid black;
     text-align: right;
     padding-left: .5rem;
-
     :first-child {
       color: gray;
     }
+`;
 
+const Wrapper = styled.div`
+display: flex;
+flex-direction: row;
 `;
 
 const Button = styled.button`
@@ -62,7 +80,6 @@ const Button = styled.button`
     border-radius: 1rem;
     margin: 1.5rem;
     transition: 1s ease;
-
     :hover{
         color: teal;
         transform: scale(1.1, .8);
@@ -158,13 +175,11 @@ export default class DayCard extends Component {
 //api/app/addChild --> needs parentId and fullname
 
   addChild = (event) => {
-    // event.preventDefault();
+    event.preventDefault();
     const userdata = JSON.parse(localStorage.getItem('userdata'));
-    if (this.state.child === ""){
-        return alert('You didn\'t enter a childs name' )
-    }
-    else{
-
+    if(this.state.addChild === ""){
+      return alert("You did not enter a child's name.")
+    } else {
     axios
     .post(`${url}/api/app/addchild`, {
       fullName: this.state.addChild,
@@ -177,9 +192,10 @@ export default class DayCard extends Component {
     })
 
     .then(res => {
-      console.log('Its working', res);
+      console.log('Its working', res.data);
       this.setState({
-        children: [...this.state.children, res.data]
+      children: [...this.state.children, res.data],
+      addChild: ''
       })
     })
 
@@ -188,6 +204,7 @@ export default class DayCard extends Component {
     })
   }
   }
+
 
   postEntry = (event) => {
       event.preventDefault();
@@ -229,59 +246,10 @@ export default class DayCard extends Component {
     if (!this.state.day) {
       return <h1>Loading dietary information...</h1>;
     }
+
     return (
-        <>
-      <Form>
-      <Button onClick = {this.postEntry}> Clicka Me </Button>
-      <Button onClick = {this.addChild}> Add Child </Button>
-      <form>
-        <Input 
-           placeholder = "Add Child..."
-           type = "text"
-           value = {this.state.addChild}
-           name = "addChild"
-           onChange = {this.handleChanges}/>
-      </form>
-            <label>
-                <Select name = "fullName" value={this.state.fullName} onChange={this.handleChanges}>
-                    <option value ="" disabled selected hidden>Select Child...</option>
-                    {this.state.children.map(child => {
-                      return <option value = {child.fullName}> {child.fullName} </option>
-                    })}
-                </Select>
-            </label>
-
-            <label>
-                <Select name = "mealTime" value={this.state.mealTime} onChange={this.handleChanges}>
-                    <option value ="" disabled selected hidden>Select Meal...</option>
-                    <option value="breakfast">Breakfast</option>
-                    <option value="lunch">Lunch</option>
-                    <option value="dinner">Dinner</option>
-                    <option value="dessert">Dessert</option>
-                </Select>
-            </label>
-
-            <label>
-                <Select name = "foodType" value = {this.state.foodType} onChange={this.handleChanges}>
-                    <option value="" disabled selected hidden>Select food type...</option>
-                    <option value="fruit">Fruit</option>
-                    <option value="vegetable">Vegetable</option>
-                    <option value="wholeGrain">Whole Grain</option>
-                    <option value="meat">Meat</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="fat">Fat</option>
-                    <option value="oil">Oil</option>
-                    <option value="treats">Treats</option>
-                </Select>
-            </label>
-
-           <Input 
-           placeholder = "Foodname..."
-           type = "text"
-           value = {this.state.foodName}
-           name = "foodName"
-           onChange = {this.handleChanges}/>
-
+    <Wrapper>
+      <MealWrapper>
       <Div>
            {this.state.entry.map((ent)=>{
               return (
@@ -295,8 +263,66 @@ export default class DayCard extends Component {
               )
           })}
       </Div>
-      </Form>
-      </>
+      </MealWrapper>
+
+      <ChildForm>
+        <h2>Create a child here.</h2>
+          <Input 
+            placeholder = "Add Child..."
+            type = "text"
+            value = {this.state.addChild}
+            name = "addChild"
+            onChange = {this.handleChanges}/>
+
+          <Button onClick = {this.addChild}> Add Child </Button>
+
+        <h2>Create a Meal Entry here.</h2>
+
+          <label>
+              <Select name = "fullName" value={this.state.fullName} onChange={this.handleChanges}>
+                  <option value ="" disabled selected hidden>Select Child...</option>
+                    {this.state.children.map((child, index) => {
+                    return <option key = {index} value = {child.fullName}> {child.fullName} </option>
+                  })}
+              </Select>
+          </label>
+
+          <label>
+              <Select name = "mealTime" value={this.state.mealTime} onChange={this.handleChanges}>
+                  <option value ="" disabled selected hidden>Select Meal...</option>
+                  <option value="breakfast">Breakfast</option>
+                  <option value="lunch">Lunch</option>
+                  <option value="dinner">Dinner</option>
+                  <option value="dessert">Dessert</option>
+              </Select>
+          </label>
+
+          <label>
+              <Select name = "foodType" value = {this.state.foodType} onChange={this.handleChanges}>
+                  <option value="" disabled selected hidden>Select food type...</option>
+                  <option value="fruit">Fruit</option>
+                  <option value="vegetable">Vegetable</option>
+                  <option value="wholeGrain">Whole Grain</option>
+                  <option value="meat">Meat</option>
+                  <option value="dairy">Dairy</option>
+                  <option value="fat">Fat</option>
+                  <option value="oil">Oil</option>
+                  <option value="treats">Treats</option>
+              </Select>
+          </label>
+
+          <Input 
+          placeholder = "Foodname..."
+          type = "text"
+          value = {this.state.foodName}
+          name = "foodName"
+          onChange = {this.handleChanges}/>
+
+          <Button onClick = {this.postEntry}> Add Meal </Button>
+
+     
+      </ChildForm>
+    </Wrapper>
 
     );
   }
